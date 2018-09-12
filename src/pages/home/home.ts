894399449c9f976420ad313c2512f11016af5541
Actualizacion funcion nuevos y antiguos <>
2018-09-12 16:12:21 -0500

    Muestra el nombre

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,29 +2,45 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-   users: any[] = [];
+  users: any;
+  alumno: any;
+  alumnoArray: any;
+  nombre: any;
+  acudiente: any;
 
   constructor(
   public navCtrl: NavController,
   public restProvider: RestProvider
-  ) {}
-
-   ionViewDidLoad(){
-    this.restProvider.getUsers()
-    .subscribe(
-      (data) => {
-      this.users = data['results'];
-      },
-      (error) =>{
-         console.error(error);
-      }
-    )
+  ) {
+    //this.getUsers();
+    this.getAlumno();
   }
 
+  getUsers() {
+    this.restProvider.getUsers()
+    .then(data => {
+      this.users = data;
+      console.log(this.users);
+    });
+  }
+
+  getAlumno() {
+    this.restProvider.getAlumno()
+    .then(data => {
+      this.alumno = data.Alumno;
+      this.alumnoArray = data.Alumno;
+      this.nombre = data.Alumno.nombres;
+      this.acudiente = data.Alumno.acudiente;
+      console.log(this.alumno);
+      console.log(this.alumnoArray);
+
+    })
+  }
 }
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -4,11 +4,30 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RestProvider {
 
-  constructor(private http: HttpClient
-  ) {}
+	apiUrl = 'https://jsonplaceholder.typicode.com';
+	apiUrl2 = 'http://sing.com.co/colina/api/alumnos/view/1?apiKey=9tMS1A8G5QY4N84';
 
-getUsers() {
-	return this.http.get('http://sing.com.co/colina/api/alumnos/view/1?apiKey=9tMS1A8G5QY4N84');
-} 
+	constructor(private http: HttpClient) {
+		console.log('Hello RestServiceProvider Provider');
+	}
 
+	getUsers() {
+	  return new Promise(resolve => {
+	    this.http.get(this.apiUrl+'/users').subscribe(data => {
+	      resolve(data);
+	    }, err => {
+	      console.log(err);
+	    });
+	  });
+	}
+
+	getAlumno() {
+	  return new Promise(resolve => {
+	    this.http.get(this.apiUrl2).subscribe(data => {
+	      resolve(data);
+	    }, err => {
+	      console.log(err);
+	    });
+	  });
+	}
 }
